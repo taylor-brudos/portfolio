c770b8cee9dca0a4e580a09a79a81219f47f34ad
taylor-brudos <taylor.brudos@gmail.com>
2019-03-03 18:48:48 -0800

    home and about pages

--- main/apps/first_app/views.py
+++ main/apps/first_app/views.py
@@ -1,9 +1,7 @@
 from django.shortcuts import render, HttpResponse, redirect
 
 def index(request):
-    context = {
-        "email" : "blog@gmail.com",
-        "name" : "mike"
-    }
-    return render(request, "first_app/index.html", context)
-    
+    return render(request, "first_app/index.html")
+
+def about(request):
+    return render(request, "first_app/about.html")
